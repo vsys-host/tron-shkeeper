f3832bf0bba37a43ab873ef41ef8b3c9b72fc9f2
dima <sdv@inet.ua>
2023-01-20 16:03:24 +0200

    WAL fix

--- app/tasks.py
+++ app/tasks.py
@@ -87,9 +87,9 @@ def post_payout_results(data, symbol):
 @celery.task()
 def refresh_trc20_balances(symbol):
     con = sqlite3.connect(config["BALANCES_DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES, isolation_level=None)
+    con.execute('pragma journal_mode=wal')
     with con:
         cur = con.cursor()
-        cur.execute('PRAGMA journal_mode=wal')
         try:
             cur.execute('BEGIN IMMEDIATE')
         except sqlite3.OperationalError as e:
