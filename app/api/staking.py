5044fd590cacaa5b99d5db7b7c5b0d374066463f
Dmytro Samoylenko <sdv@inet.ua>
2025-07-24 15:51:20 +0000

    Add endpoint for account staking info

--- app/api/staking.py
+++ app/api/staking.py
@@ -10,6 +10,33 @@ from tronpy import Tron
 from tronpy.keys import PrivateKey
 
 
+@staking_bp.get("/", defaults={"address": None})
+@staking_bp.get("/<address>")
+def get_resources(address):
+    if not address:
+        address = query_db2('select * from keys where type = "fee_deposit" ', one=True)[
+            "public"
+        ]
+    tron_client: Tron = ConnectionManager.client()
+    account_info = tron_client.get_account(address)
+
+    index = tron_client.get_delegated_resource_account_index_v2(address)
+    account_resource = tron_client.get_account_resource(address)
+    delegated_resources = []
+    if "toAccounts" in index:
+        for to_addr in index["toAccounts"]:
+            deleg_res = tron_client.get_delegated_resource_v2(address, to_addr)
+            if "delegatedResource" in deleg_res:
+                for i in deleg_res["delegatedResource"]:
+                    delegated_resources.append(i)
+
+    return {
+        "account_info": account_info,
+        "delegated_resources": delegated_resources,
+        "account_resource": account_resource,
+    }
+
+
 @staking_bp.post("/freeze/<int:amount>/<string:res_type>")
 def stake_trx(amount: int, res_type: Literal["ENERGY", "BANDWIDTH"]):
     main_acc_keys = query_db2(
