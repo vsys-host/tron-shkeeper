88d55ca491b83ef4ceb2fa9eb241dafa511bc47f
Dmytro Samoylenko <sdv@inet.ua>
2025-11-05 12:04:01 +0000

    Add staking info/config endpoint

--- app/api/staking.py
+++ app/api/staking.py
@@ -1,40 +1,137 @@
 from typing import Literal
 
-from app.utils import get_energy_delegator
+
+from app.utils import get_energy_delegator, get_key
+from app.schemas import KeyType
 
 from . import staking_bp
 from ..db import query_db2
 from ..connection_manager import ConnectionManager
 from ..wallet_encryption import wallet_encryption
 from ..logging import logger
+from ..config import config
 
 from tronpy import Tron
 from tronpy.keys import PrivateKey
+from tronpy.exceptions import AddressNotFound
+
+
+@staking_bp.get("/info")
+def get_staking_info():
+    """
+    Returns staking-related configuration options and account status information.
+
+    Returns:
+        dict: Contains:
+            - config: Staking-related configuration options
+            - fee_deposit_account: Fee deposit account address and status (on-chain/off-chain)
+            - energy_delegator_account: Energy delegator account address and status (on-chain/off-chain)
+    """
+    try:
+        tron_client: Tron = ConnectionManager.client()
+
+        # Get fee deposit account
+        _, fee_deposit_address = get_key(KeyType.fee_deposit)
+        fee_deposit_status = "unknown"
+        fee_deposit_info = None
+
+        if fee_deposit_address:
+            try:
+                fee_deposit_info = tron_client.get_account(fee_deposit_address)
+                fee_deposit_status = True
+            except AddressNotFound:
+                fee_deposit_status = False
+
+        # Get energy delegator account (might be different from fee_deposit)
+        energy_delegator_priv, energy_delegator_address = get_energy_delegator()
+        energy_delegator_status = None
+        energy_delegator_info = None
+
+        if energy_delegator_address:
+            try:
+                energy_delegator_info = tron_client.get_account(
+                    energy_delegator_address
+                )
+                energy_delegator_status = True
+            except AddressNotFound:
+                energy_delegator_status = False
+
+        # Collect staking-related configuration
+        staking_config = {
+            "energy_delegation_mode": config.ENERGY_DELEGATION_MODE,
+            "energy_delegation_mode_allow_burn_trx_for_bandwidth": config.ENERGY_DELEGATION_MODE_ALLOW_BURN_TRX_FOR_BANDWITH,
+            "energy_delegation_mode_allow_burn_trx_on_payout": config.ENERGY_DELEGATION_MODE_ALLOW_BURN_TRX_ON_PAYOUT,
+            "energy_delegation_mode_allow_additional_energy_delegation": config.ENERGY_DELEGATION_MODE_ALLOW_ADDITIONAL_ENERGY_DELEGATION,
+            "energy_delegation_mode_energy_delegation_factor": float(
+                config.ENERGY_DELEGATION_MODE_ENERGY_DELEGATION_FACTOR
+            ),
+            "energy_delegation_mode_separate_balance_and_energy_accounts": config.ENERGY_DELEGATION_MODE_SEPARATE_BALANCE_AND_ENERGY_ACCOUNTS,
+            "energy_delegation_mode_energy_account_pub_key": config.ENERGY_DELEGATION_MODE_ENERGY_ACCOUNT_PUB_KEY,
+            "sr_voting": config.SR_VOTING,
+            # tmp fix for TypeError: Object of type SrVote is not JSON serializable
+            "sr_votes": str(config.SR_VOTES),
+            "sr_voting_allow_burn_trx": config.SR_VOTING_ALLOW_BURN_TRX,
+        }
+
+        return {
+            "status": "success",
+            "config": staking_config,
+            "fee_deposit_account": {
+                "address": fee_deposit_address,
+                "is_active": fee_deposit_status,
+                "info": fee_deposit_info,
+            },
+            "energy_delegator_account": {
+                "address": energy_delegator_address,
+                "is_active": energy_delegator_status,
+                "info": energy_delegator_info,
+                "is_externally_managed": energy_delegator_priv is None,
+                "is_same_as_fee_deposit": energy_delegator_address
+                == fee_deposit_address,
+            },
+        }
+    except Exception as e:
+        logger.exception("Error getting staking info")
+        return {
+            "status": "error",
+            "msg": str(e),
+        }
 
 
 @staking_bp.get("/", defaults={"address": None})
 @staking_bp.get("/<address>")
 def get_resources(address):
-    if not address:
-        _, address = get_energy_delegator()
-    tron_client: Tron = ConnectionManager.client()
-    account_info = tron_client.get_account(address)
+    try:
+        if not address:
+            _, address = get_energy_delegator()
+        tron_client: Tron = ConnectionManager.client()
+        account_info = tron_client.get_account(address)
 
-    index = tron_client.get_delegated_resource_account_index_v2(address)
-    account_resource = tron_client.get_account_resource(address)
-    delegated_resources = []
-    if "toAccounts" in index:
-        for to_addr in index["toAccounts"]:
-            deleg_res = tron_client.get_delegated_resource_v2(address, to_addr)
-            if "delegatedResource" in deleg_res:
-                for i in deleg_res["delegatedResource"]:
-                    delegated_resources.append(i)
-
-    return {
-        "account_info": account_info,
-        "delegated_resources": delegated_resources,
-        "account_resource": account_resource,
-    }
+        index = tron_client.get_delegated_resource_account_index_v2(address)
+        account_resource = tron_client.get_account_resource(address)
+        delegated_resources = []
+        if "toAccounts" in index:
+            for to_addr in index["toAccounts"]:
+                deleg_res = tron_client.get_delegated_resource_v2(address, to_addr)
+                if "delegatedResource" in deleg_res:
+                    for i in deleg_res["delegatedResource"]:
+                        delegated_resources.append(i)
+        return {
+            "account_info": account_info,
+            "delegated_resources": delegated_resources,
+            "account_resource": account_resource,
+        }
+    except AddressNotFound:
+        return {
+            "status": "error",
+            "msg": "account not found on-chain",
+            "details": {"address": address},
+        }
+    except Exception as e:
+        return {
+            "status": "error",
+            "msg": str(e),
+        }
 
 
 @staking_bp.post("/freeze/<int:amount>/<string:res_type>")
